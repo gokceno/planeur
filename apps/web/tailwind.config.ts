dd9354d924aec6ff18c41d1cce246bee41157b37
Gokcen Ogutcu <gokcen@brewww.com>
2024-09-07 06:37:26 +0300

    fix: added missing colspans, removed light colors

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -8,6 +8,8 @@ export default {
     "col-span-3",
     "col-span-4",
     "col-span-5",
+    "col-span-6",
+    "col-span-7",
     {
       pattern:
         /bg-(slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(50|100|200|300|400|500|600|700|800|900)/,
